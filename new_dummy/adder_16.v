1260dc399b93f0980ea9c9c6dd76149f57779d69
Nikhil Ajith <nikhil.ajith95@gmail.com>
2021-05-10 20:28:43 +0530

    dummy add

--- new_dummy/adder_16.v
+++
@@ -1,9 +0,0 @@
-module adder_16 (in1, in2 , out);
-
-	output  [15:0]  out;
-	
-	input  [15:0] in1, in2;
-	
-	assign out = in1+in2;
-	
-endmodule
--- new_dummy/comparator.v
+++
@@ -1,8 +0,0 @@
-module comparator(ip0, ip1, equal); 
-
-	output equal;
-	
-	input  [15:0] ip0, ip1;
-	
-	assign equal = (ip0 == ip1) ? 1'b1 : 1'b0;
-endmodule
